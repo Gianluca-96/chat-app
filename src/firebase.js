f3a9b4a72551461edb240a640dc58a1ed99a4b32
Gianluca-96 <gianluca.agostino1@outlook.it>
2022-12-15 22:39:21 +0100

    complete navbar - add firebase - start signin

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_IM7NWys5i2nFWummeoAp6Vij4-xnnsw",
+  authDomain: "chat-app-d03a4.firebaseapp.com",
+  projectId: "chat-app-d03a4",
+  storageBucket: "chat-app-d03a4.appspot.com",
+  messagingSenderId: "965464190016",
+  appId: "1:965464190016:web:da525a6cdfa2b5d74e83a5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
